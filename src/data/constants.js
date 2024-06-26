465f216b9a0a6546b38bab6ada093c47d19c50af
unknown <sakshikhule3010@gmail.com>
2024-06-26 06:34:47 +0530

    Portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,165 @@
+export const Bio = {
+    name: "Sakshi Khule",
+    roles: [
+      "Full Stack Developer",
+      
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/sakshikhule",
+    resume:
+      "https://drive.google.com/file/d/1lt1xLQTKwnLD4Y5zSJhSTPlV3niGI1Jv/view?usp=drivesdk",
+    
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+       
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+       
+      ],
+    },
+    {
+      title: "Android",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+       
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+       
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+       
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    
+    role: "java Intern",
+    company: "Seema Technology",
+    date: "Feb 2023 - March 2023",
+    desc: "Working on java.",
+    skills: [
+      "MYSQL",
+      "JAVA",
+      "DataBase",
+      "jDBC"
+    ],
+ 
+  },
+ 
+    
+ 
+];
+
+export const education = [
+  {
+    id: 0,
+    
+    school: "METs Bhujbal Knowledge City",
+    date: "Oct 2020 - june 2024",
+    grade: "8.71 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Information Technology ",
+    degree: "Bachelor of Technology - BE,IT",
+  },
+  {
+    id: 1,
+  
+    school: "Canossa Convent High School",
+    date: "Apr 2020 ",
+    grade: "80.0%",
+    desc: "I completed my class 12 high school education ",
+    degree: "XII",
+  },
+  {
+    id: 2,
+  
+    school: "Canossa Convent High School",
+    date: "Apr 2018",
+    grade: "90.4%",
+    desc: "I completed my class 10 education",
+    degree: "X",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Hotel Management",
+    date: "Jun 2023 ",
+    description:
+      "Manages Hotel",
+    
+    
+  },
+  {
+    id: 0,
+    title: "MusicApp",
+    date: "Apr 2024",
+    description:
+      "Developed a full-stack web application that allows users to search for, play, and pause their favorite music on demand.",
+   
+  },
+]
